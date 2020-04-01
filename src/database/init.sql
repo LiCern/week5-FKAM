00f4fbb28b5a16731c83ee0c630614141164a7eb
Joe Jackson <joe.m.jackson@live.co.uk>
2020-04-01 16:06:22 +0100

    Define schemas
    Co-authored-by: Joepock123 <joe.m.jackson@live.co.uk>
    Co-authored-by: glrta <to.gio@pm.me>
    Co-authored-by: itsina96 <itsina96@gmail.com>
    Co-authored-by: Roger-Heathcote <github@technicalbloke.com>

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,6 +1,37 @@
 BEGIN;
 
--- initialise your tables here
--- insert some test data if you like
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  postcode VARCHAR(8)
+);
+
+CREATE TABLE category (
+  id SERIAL PRIMARY KEY,
+  category VARCHAR(20)
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  title VARCHAR(255) NOT NULL,
+  post TEXT NOT NULL,
+  time TIMESTAMPTZ,
+  category INTEGER REFERENCES category(id)
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  postcode VARCHAR(8)
+);
+
+INSERT INTO category (category) VALUES
+  ('Food'),
+  ('Toiletries'),
+  ('Household'),
+  ('Other')
+;
 
 COMMIT;
