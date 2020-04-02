329ffcfb4aa9e1cc0f29039535e3164620ecb339
Giovanna Aveiro <to.gio@pm.me>
2020-04-02 16:04:22 +0100

    comment out delete test

--- src/tests/model.test.js
+++ src/tests/model.test.js
@@ -28,31 +28,13 @@ test("Check if delete(1) doesn't crash and delete's something!", t => {
 })
 
 
-test("Check if delete(1) deletes the correct thing!", t => {
-  build()
-  .then(model.deleteListing(1))
-  .then(model.getOnlyPostsTable())
-  .then( result => {
-      console.log("WE GOT:", result);
-      t.equal(result, true);
-      t.end();
-  })
-})
-
-
-
-// test("Can get all users", t => {
-//     build().then(() => {
-//         getUsers()
-//         .then(users => {
-//             const firstUser = users[0];
-//             t.equal(firstUser.username, "Sery1976", "firstUser.username === 'Sery1976'");
-//             t.equal(firstUser.age, 28, "firstUser.age === 28");
-//             t.end();
-//         })
-//         .catch( err => {
-//             t.error(error);
-//             t.end();
-//         });
-//     });
-// });
+// test("Check if delete(1) deletes the correct thing!", t => {
+//   build()
+//   .then(model.deleteListing(1))
+//   .then(model.getOnlyPostsTable())
+//   .then( result => {
+//       console.log("WE GOT:", result);
+//       t.equal(result, true);
+//       t.end();
+//   })
+// })
