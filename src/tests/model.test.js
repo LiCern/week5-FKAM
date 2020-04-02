74cb8a3b2ef72719f27679f625d7d1d85ba60b90
Giovanna Aveiro <to.gio@pm.me>
2020-04-02 15:46:15 +0100

    add broken delete test for investigation

--- src/tests/model.test.js
+++ src/tests/model.test.js
@@ -1,6 +1,58 @@
+const model = require("../model");
 const test = require("tape");
+const build = require("../database/testbuild");
+
 
 test("Tape is working!", t => {
   t.equal(1, 1);
   t.end();
 });
+
+test("Check model is exporting the function deleteListing", t => {
+  t.equal("deleteListing" in model , true);
+  t.end();
+});
+
+test("Check deleteListing returns true", t => {
+  t.equal("deleteListing" in model , true);
+  t.end();
+});
+
+test("Check if delete(1) doesn't crash and delete's something!", t => {
+    build()
+    .then( () => {
+        let result = model.deleteListing(1)
+        t.equal(result, true);
+        t.end();
+    })
+})
+
+
+test("Check if delete(1) deletes the correct thing!", t => {
+  build()
+  .then(model.deleteListing(1))
+  .then(model.getOnlyPostsTable())
+  .then( result => {
+      console.log("WE GOT:", result);
+      t.equal(result, true);
+      t.end();
+  })
+})
+
+
+
+// test("Can get all users", t => {
+//     build().then(() => {
+//         getUsers()
+//         .then(users => {
+//             const firstUser = users[0];
+//             t.equal(firstUser.username, "Sery1976", "firstUser.username === 'Sery1976'");
+//             t.equal(firstUser.age, 28, "firstUser.age === 28");
+//             t.end();
+//         })
+//         .catch( err => {
+//             t.error(error);
+//             t.end();
+//         });
+//     });
+// });
