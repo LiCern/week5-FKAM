404c308982115a33e0698a1456391419ff50fd92
Ina Yoon <itsina96@gmail.com>
2020-04-03 11:31:55 +0100

    add error handler in server Relates #46

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(router);
 
+
 process.on('unhandledRejection', (error) => {
     console.error(error);
     process.exit(1);
 })
-
+ 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
